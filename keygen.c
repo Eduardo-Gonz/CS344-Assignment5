cc5c15ec35fb4bcd43fde719ec25146f61913e4f
Eduardo-Gonz <erugonz249@gmail.com>
2021-11-28 13:39:10 -0800

    Implement functionality of keygen.c

--- keygen.c
+++ keygen.c
@@ -2,13 +2,42 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <time.h>
 
+
+void generateKey(int keyLength) {
+    char key[keyLength + 1];
+
+    //Generate random chars for the key
+    for(int i = 0; i < keyLength; i++) {
+        int randNum = random() % 27 + 1;
+
+        if(randNum < 27) {
+            randNum += 64; //ascii value of letters A-Z
+        }
+        else {
+            randNum = 32; //ascii value of space
+        }
+
+        char randChar = randNum;
+        key[i] = randChar;
+    }
+
+    key[keyLength] = '\n'; //append new line char to end of key
+
+    printf("%s", key);
+}
 
 
 int main(int argc, char *argv[]) {
-
     srandom(time(NULL));
-    int randNum = random() % 100000;
 
+    if(argc != 2) {
+        printf("Incorrect number of arguments");
+        return EXIT_SUCCESS;
+    }
+
+    int length = atoi(argv[1]);
+    generateKey(length);
     return EXIT_SUCCESS;
 }
